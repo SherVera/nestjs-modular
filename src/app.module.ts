648e400c76365e70753ba8e1861c0bd5c77eefd0
Sher Vera <vera8german@gmail.com>
2021-06-17 00:45:56 -0400

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@
 import { Module, HttpModule, HttpService } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
-
+// separar importacion interna
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
